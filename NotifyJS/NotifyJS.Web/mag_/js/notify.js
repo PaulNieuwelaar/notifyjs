45fe5eafbd4af2e2c8d7c934f96be3274f743904
Paul Nieuwelaar <14048382+PaulNieuwelaar@users.noreply.github.com>
2021-12-14 08:14:39 +1300

    v2

--- NotifyJS/NotifyJS.Web/mag_/js/notify.js
+++ NotifyJS/NotifyJS.Web/mag_/js/notify.js
@@ -1,23 +1,14 @@
-// Notify.js v1.1 - Copyright Paul Nieuwelaar Magnetism 2016
-// Download the latest version from https://github.com/PaulNieuwelaar/notifyjs
+/* v2.0.0 | (c) Paul Nieuwelaar Magnetism Apps Limited | https://github.com/PaulNieuwelaar/notifyjs */
 
 /*
     // Add a notification with a button and a link (inline html in the message too)
     Notify.add("Would you like to create a new <b>Sale</b>?", "QUESTION", "sale",
-        [{
-            type: "button",
-            text: "Create Sale",
-            callback: function () {
-                Notify.add("Sale created successfully!", "SUCCESS", "sale", null, 3);
-            }
-        },
-        {
-            type: "link",
-            text: "Not now",
-            callback: function () {
-                Notify.remove("sale");
-            }
-        }]);
+        [new Notify.Button("Create Sale", function () {
+            Notify.add("Sale created successfully!", "SUCCESS", "sale", null, 3);
+        }),
+        new Notify.Link("Not now", function () {
+            Notify.remove("sale");
+        })]);
 
     // Add a notification which disappears after 5 seconds
     Notify.add("Workflow executed successfully!", "SUCCESS", "success", null, 5);
@@ -30,7 +21,13 @@
 */
 
 var Notify = Notify || {};
-var $ = $ || parent.$;
+
+Notify._context = window.document;
+Notify._jQuery = top.jQuery || parent.jQuery || window.jQuery;
+
+Notify.$ = function (selector, context) {
+    return Notify._jQuery(selector, context || Notify._context);
+}
 
 Notify._notifications = [];
 Notify._timeStamp = null;
@@ -40,60 +37,25 @@ Notify._crmFormHeaderId = "formHeaderContainer"; // This is probably the only th
 Notify._crmViewHeaderId = "crmContentPanel"; // And this, but it's cool
 
 // message = (optional) what is displayed in the notification bar
-// level = (optional) ERROR, WARNING, INFO, SUCCESS, QUESTION, or LOADING. If not included, no image will display
+// level = (optional) ERROR, WARNING, INFO, SUCCESS, QUESTION, SUCCESS, or LOADING. If not included, no image will display
 // uniqueId = (optional) unique ID for this notification
 // buttons = (optional) array of objects, each object must have a 'text' attrbute, a 'callback' function attribute, and a 'type' attribute of 'link' or 'button'
 // durationSeconds = (optional) after how long should the notification disappear
-Notify.add = function (message, level, uniqueId, buttons, durationSeconds) {
-    if (!Notify._initialised) {
-        var $notify = $("<div>", { id: "notifyWrapper" });
-        $notify.append($("<div>", { id: "notify", class: "notify", size: "3", maxheight: "51", class: "notify" }).css("display", "block"));
+// bgColor = hex code for the notification background. Defaults to Classic CRM yellow: "#FFF19D"
+// textColor = hex code for the notification text, button outlines, link colors, X color, etc. Defaults to Grey: #444444
+Notify.add = function (message, level, uniqueId, buttons, durationSeconds, bgColor, textColor) {
+    bgColor = bgColor || "#FFF19D"; // Classic CRM yellow
+    textColor = textColor || "#444444"; // Grey
+    uniqueId = uniqueId ? (uniqueId + "").toLowerCase() : ""; // Accepts non-strings and undefined
 
-        // Try get the form header
-        var $header = $("#" + Notify._crmFormHeaderId);
-        if ($header.length > 0) {
-            $header.append($notify);
-        }
-
-        if ($header.length == 0) {
-            $ = parent.$;
-
-            // Try get the form header again (2015 SP1)
-            var $header = $("#" + Notify._crmFormHeaderId);
-            if ($header.length > 0) {
-                $header.append($notify);
-            }
-
-            // If not form header, might be a view, so try get the view header
-            if ($header.length == 0) {
-                $header = $("#" + Notify._crmViewHeaderId);
-                if ($header.length > 0) {
-                    $header.prepend($notify);
-                }
-            }
-        }
-
-        if ($header.length > 0) {
-            // Load the style sheet
-            var baseUrl = Xrm.Page.context.getClientUrl();
-            $("<link/>", { rel: "stylesheet", href: baseUrl + "/WebResources/" + Notify._prefix + "/css/notify.css" }).appendTo('head');
-
-            Notify._initialised = true;
-        }
-        else {
-            // Broken most likely from a rollup/update - just need to find the new header ID (hopefully)
-            console.log("Notify: CRM header element: '" + Notify._crmHeaderId + "' does not exist.");
-            return;
-        }
-    }
-
-    // Accepts non-strings and undefined
-    uniqueId = uniqueId ? (uniqueId + "").toLowerCase() : "";
+    Notify._initialise();
 
     var notification = {
         id: uniqueId,
         severity: level,
-        buttons: buttons
+        buttons: buttons,
+        bgColor: bgColor,
+        textColor: textColor
     };
 
     // Update or append the new notification
@@ -110,31 +72,47 @@ Notify.add = function (message, level, uniqueId, buttons, durationSeconds) {
     }
 
     // Unhide the notify wrapper if this is the first notification
-    $("#notifyWrapper").show();
+    Notify.$("#notifyWrapper").show();
 
     // If the element exists remove it before recreating it
-    $("#notifyNotification_" + uniqueId).remove();
+    Notify.$("#notifyNotification_" + uniqueId).remove();
 
     // Create all the elements for this notification
-    var $elem = $("<div>", { id: "notifyNotification_" + uniqueId, class: "notify-notification" }).hide().prependTo($("#notify"));
-    var $table = $("<table>", { cellpadding: "0", cellspacing: "0" }).css("width", "100%").appendTo($elem);
-    var $tr = $("<tr>").appendTo($table);
-    if (level && ["INFO", "WARNING", "ERROR", "SUCCESS", "QUESTION", "LOADING"].indexOf(level) !== -1) {
-        var $imgTd = $("<td>", { valign: "top" }).css("width", "23px").appendTo($tr);
-        var imgType = level == "ERROR" ? "crit" : level == "WARNING" ? "warn" : level == "INFO" ? "info" : level == "SUCCESS" ? "tick" : level == "QUESTION" ? "ques" : "load";
-        var $img = $("<div>");
+    var $elem = Notify.$("<div>", { id: "notifyNotification_" + uniqueId, class: "notify-notification" }).css("color", textColor).css("background-color", bgColor).hide().prependTo(Notify.$("#notify"));
+    var $table = Notify.$("<table>", { cellpadding: "0", cellspacing: "0" }).css("width", "100%").appendTo($elem);
+    var $tr = Notify.$("<tr>").appendTo($table);
+    if (level && ["INFO", "WARNING", "ERROR", "SUCCESS", "QUESTION", "LOADING", "SEARCH"].indexOf(level) !== -1) {
+        var $imgTd = Notify.$("<td>", { valign: "top" }).css("width", "22px").appendTo($tr);
+        var imgType = level == "ERROR" ? "crit" : level == "WARNING" ? "warn" : level == "INFO" ? "info" : level == "SUCCESS" ? "tick" : level == "QUESTION" ? "ques" : level == "LOADING" ? "load" : "find";
+        var $img = Notify.$("<div>");
         $img.addClass("notify-image notify-image-" + imgType);
         $img.appendTo($imgTd);
     }
-    var $textTd = $("<td>").appendTo($tr);
-    var $close = $("<a>", { title: "Dismiss", class: "notify-close" }).click(function () { Notify.remove(uniqueId); });;
+    var $textTd = Notify.$("<td>", { class: "notify-text" }).appendTo($tr);
+    var $close = Notify.$("<a>", { title: "Dismiss", class: "notify-close" }).css("color", textColor).click(function () { Notify.remove(uniqueId); });
+    $close.append("&times;");
     $textTd.append($close);
     $textTd.append(message || "");
     if (buttons && buttons.length > 0) {
         for (var i = 0; i < buttons.length; i++) {
             var b = buttons[i];
-            var $button = $("<a>", { class: b.type == "link" ? "notify-link" : "notify-button" }).click(b.callback);
+            var $button = Notify.$("<a>", { class: b.type == "link" ? "notify-link" : "notify-button" });
+            $button.css("color", textColor).click(b.callback);
             $button.append(b.text || "");
+
+            if (b.type != "link") {
+                var rgb = Notify._hexToRgb(textColor);
+                if (rgb != null) {
+                    $button.hover(function () {
+                        // On hover
+                        Notify.$(this).css("background-color", "rgba(" + rgb.r + "," + rgb.g + "," + rgb.b + ",0.2)");
+                    }, function () {
+                        // Off hover
+                        Notify.$(this).css("background-color", "");
+                    });
+                }
+            }
+
             $textTd.append($button);
         }
     }
@@ -164,9 +142,9 @@ Notify.remove = function (uniqueId) {
 
     // If no ID specified, remove all notifications
     if (uniqueId === null || uniqueId === undefined) {
-        $("#notifyWrapper").slideUp(500, function () {
+        Notify.$("#notifyWrapper").slideUp(500, function () {
             for (var i = 0; i < Notify._notifications.length; i++) {
-                $("#notifyNotification_" + Notify._notifications[i].id).remove();
+                Notify.$("#notifyNotification_" + Notify._notifications[i].id).remove();
             }
 
             Notify._notifications = [];
@@ -187,14 +165,107 @@ Notify.remove = function (uniqueId) {
 
         if (Notify._notifications.length == 0) {
             // If that was the last notification hide the notify wrapper
-            $("#notifyWrapper").slideUp(500, function () {
+            Notify.$("#notifyWrapper").slideUp(500, function () {
                 // Delete the notification once hidden
-                $("#notifyNotification_" + uniqueId).remove();
+                Notify.$("#notifyNotification_" + uniqueId).remove();
             });
         }
         else {
             // Hide and Delete the element
-            $("#notifyNotification_" + uniqueId).slideUp(500, function () { $(this).remove(); });
+            Notify.$("#notifyNotification_" + uniqueId).slideUp(500, function () { Notify.$(this).remove(); });
         }
     }
+}
+
+Notify._initialise = function () {
+    if (!Notify._initialised) {
+        // Remove any leftover wrappers
+        Notify.$("#notifyWrapper").remove();
+        Notify.$("#notifyWrapper", parent.document).remove();
+
+        var $notify = Notify.$("<div>", { id: "notifyWrapper" });
+        $notify.append(Notify.$("<div>", { id: "notify", class: "notify", size: "3", maxheight: "51" }).css("display", "block"));
+
+        // Try get the form header
+        var $header = Notify.$("#" + Notify._crmFormHeaderId);
+        if ($header.length > 0) {
+            $header.append($notify);
+        }
+        else {
+            // Try get the form header from the parent
+            var $header = Notify.$("#" + Notify._crmFormHeaderId, parent.document);
+            if ($header.length > 0) {
+                Notify._context = parent.document; // This is because the notifications are now in the parent
+                $header.append($notify);
+            }
+            else {
+                // If not form header, might be a view, so try get the view header
+                $header = Notify.$("#" + Notify._crmViewHeaderId, parent.document);
+                if ($header.length > 0) {
+                    Notify._context = parent.document; // This is because the notifications are now in the parent
+                    $header.prepend($notify);
+                }
+                else {
+                    // v9+ Unified Interface form
+                    //$header = Notify.$("#tab-section", parent.document);
+                    $header = Notify.$("div[data-id='form-header']", parent.document);
+                    if ($header.length > 0) {
+                        Notify._context = parent.document; // This is because the notifications are now in the parent
+                        $header.before($notify);
+                    }
+                    else {
+                        // v9+ Unified Interface view
+                        $header = Notify.$("section[data-id^=dataSetRoot] > div", parent.document).first();
+                        //$header = Notify.$("[data-id^=commandBar]", parent.document);
+                        if ($header.length > 0) {
+                            Notify._context = parent.document; // This is because the notifications are now in the parent
+                            $header.after($notify);
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($header.length > 0) {
+            // Load the style sheet
+            var baseUrl = Xrm.Page.context.getClientUrl();
+            Notify.$("<link/>", { rel: "stylesheet", href: baseUrl + "/WebResources/" + Notify._prefix + "/css/notify.css" }).appendTo($notify);
+
+            Notify._initialised = true;
+        }
+        else {
+            // Broken most likely from a rollup/update - just need to find the new header ID (hopefully)
+            console.log("Notify: CRM header element: '" + Notify._crmFormHeaderId + "' does not exist.");
+            return;
+        }
+    }
+}
+
+Notify._hexToRgb = function (hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+Notify._Button = function (type, text, callback) {
+    this.type = type;
+    this.text = text;
+    this.callback = callback;
+}
+
+Notify.Button = function (text, callback) {
+    Notify._Button.call(this, "button", text, callback);
+}
+
+Notify.Link = function (text, callback) {
+    Notify._Button.call(this, "link", text, callback);
 }
